72c54e90ebad8858138c0d7320da609455d83252
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-12-18 19:06:51 +0100

    copy is done

--- data/text.js
+++ data/text.js
@@ -1,12 +1,12 @@
 const TEXT = {
     homePage: {
         title: "Zhanna D'art",
-        paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has"
+        paragraph: "Velkommen til min salong! Her er jeg stolt av å tilby toppkvalitets hårtjenester til deg. Jeg gjør alltid mitt beste for å gi deg det perfekte utseendet som matcher din personlige stil."
     },
     about: {
         title: "Om meg",
-        paragraphOne: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled itLorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        paragraphTwo: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled itLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        paragraphOne: "Hei der! Mitt navn er Zhanna og jeg er en profesjonell frisør med over 20 års erfaring i bransjen. Jeg brenner for å hjelpe kundene mine til å føle seg trygge og vakre ved å lage personlige frisyrer som passer deres individuelle behov og preferanser.",
+        paragraphTwo: "Jeg har fått omfattende opplæring i et bredt spekter av klippe-, farge- og stylingsteknikker. Jeg streber hele tiden etter å holde meg oppdatert på de siste trendene og teknikkene for å kunne yte kundene mine best mulig service.",
         
     },
     work: {
