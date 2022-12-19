9a01d1dd8546f9b693a2076910d215eaffd7d354
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-12-19 16:21:41 +0100

    ff

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -7,7 +7,7 @@ import { motion as m } from 'framer-motion'
 function ImageSlider({urlBefore, urlAfter}) {
   return (
     <m.div className='carousel'>
-        <m.div className='inner-carousel' drag="x" dragConstraints={{right: 0, left: -200}}>
+        <m.div className='inner-carousel' drag="x" dragConstraints={{right: 0, left: -280}}>
             <m.div className='carousel-item'>
                 <Image src={urlBefore} alt="hairstyle" fill />
             </m.div>
