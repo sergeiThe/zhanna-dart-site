597fbc79d62ced625d03a79cc67240e331d7b892
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-12-19 13:04:32 +0100

    services animation

--- components/ServiceList.jsx
+++ components/ServiceList.jsx
@@ -1,22 +1,72 @@
 import React from "react";
 import services from "../data/services";
+import { motion as m } from "framer-motion";
+
+const listVariants = {
+    initialState: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+            delayChildren: 2,
+        },
+    },
+};
+
+const listItemVariants = {
+    initialState: { opacity: 0, y: 15 },
+    show: { opacity: 1, y: 0, transition: {
+        duration: 1,
+        ease: "easeOut",
+        delayChildren: 0.5
+    } },
+};
+
+const leftItemVariants = {
+    initialState: { opacity: 0, x: -35 },
+    show: { opacity: 1, x: 0, transition: {
+        duration: 1,
+        ease: "easeOut"
+    } },
+};
+
+const rightItemVariants = {
+    initialState: { opacity: 0, x: 35 },
+    show: { opacity: 1, x: 0, transition: {
+        duration: 1,
+        ease: "easeOut"
+    } },
+};
 
 function ServiceList() {
-
-    const serviceList = services.map(service => {
+    const serviceList = services.map((service) => {
         return (
-            <li key={services.indexOf(service)} className="service-list-item">
-                <span>{`${service.name}`}</span>
-                <span>{`${service.fra === true ? 'fra' : ""} ${service.price} NOK`}</span>
-            </li>
-        )
-    })
-
+            <m.li
+                key={services.indexOf(service)}
+                className="service-list-item"
+                variants={listItemVariants}
+            >
+                <m.span
+                    variants={leftItemVariants}
+                >{`${service.name}`}</m.span>
+                <m.span
+                    variants={rightItemVariants}
+                >{`${service.fra === true ? "fra" : ""} ${
+                    service.price
+                } NOK`}</m.span>
+            </m.li>
+        );
+    });
 
     return (
-        <ul className="service-list">
+        <m.ul
+            className="service-list"
+            initial="initialState"
+            animate="show"
+            variants={listVariants}
+        >
             {serviceList}
-        </ul>
+        </m.ul>
     );
 }
 
