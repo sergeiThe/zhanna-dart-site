4d685ca555243bb20d21494f45821f727a0d7565
Sergei Johansen <sergeitheprogrammer@gmail.com>
2023-02-07 17:39:25 +0100

    add analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import Menu from '../components/Menu'
 import MenuContextProvider from '../store/menu-ctx'
 import BookButton from '../components/BookButton'
 import Head from 'next/head'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
@@ -20,7 +21,27 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="image/ico" href="/favicon.ico" />
+        
       </Head>
+
+
+
+      
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TKN1VDPJBY"></Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TKN1VDPJBY');
+      `}
+      </Script>
+
+
+
+
       <MenuContextProvider>
         <BookButton />
         <Hamburger />
@@ -44,3 +65,21 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+/**
+ * <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-W32FPPW');
+          `
+        }}
+      />
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W32FPPW"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+ */
