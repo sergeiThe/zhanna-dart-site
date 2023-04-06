f3c15c668c5852485c3c5380e2ef5cc6655e8b7f
Sergei Johansen <sergeitheprogrammer@gmail.com>
2023-04-06 19:50:52 +0200

    address change

--- data/text.js
+++ data/text.js
@@ -7,12 +7,12 @@ const TEXT = {
         title: "Om meg",
         paragraphOne: "Hei der! Mitt navn er Zhanna og jeg er en profesjonell frisør med over 20 års erfaring i bransjen. Jeg brenner for å hjelpe kundene mine til å føle seg trygge og vakre ved å lage personlige frisyrer som passer deres individuelle behov og preferanser.",
         paragraphTwo: "Jeg har fått omfattende opplæring i et bredt spekter av klippe-, farge- og stylingsteknikker. Jeg streber hele tiden etter å holde meg oppdatert på de siste trendene og teknikkene for å kunne yte kundene mine best mulig service.",
-        
+
     },
     work: {
         title: "Mitt arbeid",
         paragraph: "Jeg er forpliktet til å tilby et innbydende og behagelig miljø for alle mine kunder. Jeg tar meg tid til å lytte til dine bekymringer og preferanser for å skape et utseende som er perfekt for deg."
-    }, 
+    },
     services: {
         title: "Tjenester",
         paragraph1: "Jeg har alltid hatt en lidenskap for hår og skjønnhet, og jeg er dedikert til å hjelpe kundene mine til å se og føle seg best mulig.",
@@ -22,7 +22,7 @@ const TEXT = {
         title: "Kontakt",
         paragraph: "Jeg er så takknemlig for å ha muligheten til å gjøre det jeg elsker hver dag, og jeg er alltid glad for å møte nye kunder og hjelpe dem med å nå hårmålene sine. Takk for at du vurderer meg for dine hårpleiebehov. Jeg håper å se deg i stolen min snart!",
         phone: "+47 458 09 985",
-        address: "Mellomveien 18a, 7067 Trondheim",
+        address: "Mellomveien 16, 7067 Trondheim",
         facebookLink: "https://www.facebook.com/ZhannaD.Art.as",
         instagramLink: "https://www.instagram.com/zhannadart",
 
