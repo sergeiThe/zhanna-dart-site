03783eeaa9065cf21dab019caf157aa2ba9f34ae
sergei johansen <sergeitheprogrammer@gmail.com>
2023-11-28 11:14:19 +0100

    add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,26 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="image/ico" href="/favicon.ico" />
-        
+
+        {/*GOOGLE TAG*/}
+
+
+        <Script id="gtm" strategy="afterInteractive">{`(function (w, d, s, l, i) {
+          w[l] = w[l] || [];
+          w[l].push({
+            'gtm.start':
+              new Date().getTime(), event: 'gtm.js'
+          });
+          var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+          j.async = true;
+          j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NV6FBQ5W');`}</Script>
+
       </Head>
+
 
 
 
@@ -39,8 +57,10 @@ function MyApp({ Component, pageProps }) {
       `}
       </Script>
 
+      {/*GOOGLE TAG*/}
 
 
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/ns.html?id=GTM-NV6FBQ5W"></Script>
 
       <MenuContextProvider>
         <BookButton />
